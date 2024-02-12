877b8f4eafeb416be8baf0620ac4f59e513f7198
4121n <4121nnn@gmail.com>
2024-02-12 07:11:44 +0300

    solution for problem 169

---
+++ src/main/java/org/berserk4121/dailyChallenges/Solution169.java
@@ -0,0 +1,41 @@
+package org.berserk4121.dailyChallenges;
+
+public class Solution169 {
+    public int majorityElement(int[] nums) {
+        int count = 0, n = 0;
+        for(int i : nums){
+            if(count == 0){
+                n = i;
+                count++;
+            }else if(n == i){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return n;
+    }
+}
+/*
+169. Majority Element
+
+Easy
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ */
