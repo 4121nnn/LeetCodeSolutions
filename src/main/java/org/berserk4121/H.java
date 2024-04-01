507d9632dac2a5dc3e0dc25e98954dce926997b1
4121n <4121nnn@gmail.com>
2024-04-01 05:27:42 +0300

    58

--- src/main/java/org/berserk4121/H.java
+++ src/main/java/org/berserk4121/H.java
@@ -22,6 +22,26 @@ class H {
     public static void p(){ System.out.println("---------------------------"); }
 
 }
+class A {
+    public void p(int i){ System.out.println(i); }
+    public void p(char c){ System.out.println(c); }
+    public void p(long l){ System.out.println(l); }
+    public void p(boolean b){ System.out.println(b); }
+    public void p(int[] a) { System.out.println(Arrays.toString(a)); }
+    public void p(long[] a) { System.out.println(Arrays.toString(a)); }
+    public void p(boolean[] a) { System.out.println(Arrays.toString(a)); }
+    public void p(char[] a) { System.out.println(Arrays.toString(a)); }
+    public void p(int[][] m){ for(int[] a : m)  System.out.println(Arrays.toString(a)); }
+    public void p(char[][] m){ for(char[] a : m)  System.out.println(Arrays.toString(a)); }
+    public void p(long[][] m){ for(long[] a : m)  System.out.println(Arrays.toString(a)); }
+    public <T> void p(T a) { System.out.println(a); }
+    public <T> void p(T[] a) { System.out.println(Arrays.toString(a)); }
+    public <T> void p(T[][] m) { for(T[] a : m)  System.out.println(Arrays.toString(a)); }
+    public void p(PriorityQueue<int[]> pq){ for(int[] a : pq) p(a); }
+    public void p(){ System.out.println("---------------------------"); }
+
+}
+
 
 
 
