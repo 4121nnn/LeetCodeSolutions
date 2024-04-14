d851acd19b1c174ba475105a5b97c0a6e8985206
4121nnn <4121nnn@gmail.com>
2024-04-14 05:11:55 +0300

    404 description added

--- src/main/java/org/berserk4121/dailyChallenges/m04y24/Solution404.java
+++ src/main/java/org/berserk4121/dailyChallenges/m04y24/Solution404.java
@@ -3,7 +3,7 @@ package org.berserk4121.dailyChallenges.m04y24;
 public class Solution404 {
     //TC O(n)
     //SC o(h) h - height of tree
-    // public int sumOfLeftLeaves(TreeNode root) {
+    public int sumOfLeftLeaves(TreeNode root) {
         if(root == null) return 0;
     
         if(root.left != null && root.left.left == null && root.left.right == null)
@@ -12,3 +12,31 @@ public class Solution404 {
         return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
     }
 }
+/*
+404. Sum of Left Leaves
+
+Easy
+
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+ */
